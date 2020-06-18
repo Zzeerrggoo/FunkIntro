5958587ab3387432f075da50d5b84f21b9e08e93
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-06-18 12:36:49 +0300

    Add isTriangle function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
-
 // alert(isAdult(Number(prompt('Enter your age'))));
-alert(checkMultiplicity(Number(prompt('Enter dividend')), Number(prompt('Enter divisor'))));
+// alert(checkMultiplicity(Number(prompt('Enter dividend')),
+//                         Number(prompt('Enter divisor'))));
+// alert(isTriangle(5,4,3));
 
 
 /**
@@ -19,6 +19,21 @@ function isAdult(age) {
  * @param   {number} divisor
  * @returns {boolean}
  */
-function checkMultiplicity(dividend,divisor) {
-  return Number(dividend) % Number(divisor) === 0
+function checkMultiplicity(dividend, divisor) {
+  return Number(dividend) % Number(divisor) === 0;
 }
+
+/**
+ * Checking existence of triangle with given sides
+ * @param a {number} the length of one side of the triangle
+ * @param b {number} the length of one side of the triangle
+ * @param c {number} the length of one side of the triangle
+ * @returns {boolean}
+ */
+function isTriangle(a, b, c) {
+  return a + b > c && a + c > b && b + c > a;
+}
+
+
+
+
